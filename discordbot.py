6f0e859b65e2fb2965074f53459049a35aa0e8fd
talkwithA <88328849+Midorii-715@users.noreply.github.com>
2021-08-02 23:42:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,20 +24,3 @@ async def ping(ctx):
 bot.run(token)
 
 
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
