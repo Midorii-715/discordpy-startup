eb82aa98ab763c8cf4b11163b6ca825e4cf27564
Midorii <88328849+Midorii-715@users.noreply.github.com>
2021-08-09 04:34:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -97,7 +97,6 @@ async def on_message(message):
 	for i in range(len(message.content)):
 		m = message.content
 		c = m[i]
-
 		if ((c == "0") or (c == "1") or (c == "2") or (c == "3") or (c == "4") or (c == "5") or (c == "6") or (c == "7") or (c == "8") or (c == "9")):
 			if(flg_num_dice ==0):
 				num_dice.append(int(c)) 
