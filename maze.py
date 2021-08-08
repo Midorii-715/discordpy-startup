cb1d1feeeabb6e398a4bfa61840e3c1ad433e8b2
gamer <gamer>
2021-08-09 03:24:50 +0900

    Add maze test

---
+++ maze.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+class Maze:
+    def __init__(self):
+        pass
+
+    async def process(seld, message):
+        if message.content == '/maze':
+            member =  message.author
+            await message.channel.send(f"{member.mention} maze test")
+
