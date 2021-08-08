e0eefce4af6affa93a7a9ef5bec07c2591b3cd02
Midorii <88328849+Midorii-715@users.noreply.github.com>
2021-08-09 03:25:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -85,8 +85,8 @@ async def on_message(message):
             digit_roll  = 0
             flg_num_dice = 0
             for i in range(len(message.content)):
-            m = message.content
-            c = m[i]
+                m = message.content
+                c = m[i]
             
                 if (c == "0") or (c == "1") or (c == "2"): or (c == "3") or (c == "4") or (c == "5") or (c == "6") or (c == "7") or (c == "8") or (c == "9"):
                     if(flg_num_dice ==0):
