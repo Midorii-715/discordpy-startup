78549f16fe795ae814c01d6238340603b1fc4d39
Midorii <88328849+Midorii-715@users.noreply.github.com>
2021-08-08 16:57:22 +0900

    Create getteteets.py

---
+++ getteteets.py
@@ -0,0 +1,23 @@
+import tweepy
+import re
+
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+class MyStreamListener(tweepy.StreamListener):
+   def on_status(self, status):
+       if "@" in status.text:
+           pass
+       else:
+           print(status.text)
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(follow=["suzukautako"], is_async=True)
